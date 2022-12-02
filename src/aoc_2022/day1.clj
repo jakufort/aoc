9c3a50b5b6aa9d31c7d07c9faa8f08655ae3a86e
Jakub Fortunka <jakufort@gmail.com>
2022-12-02 22:08:48 +0100

    day 1

--- src/aoc_2022/day1.clj
+++ src/aoc_2022/day1.clj
@@ -1,12 +1,39 @@
 (ns aoc-2022.day1
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def input-file "./resources/day1/input")
+
+(defn group-calories [lines]
+  (partition-by #(= "" %) lines))
+
+(defn grouped-by-calories [lines]
+  (filter #(not= '("") %) (group-calories lines)))
+
+(defn to-numbers [groups]
+  (map #(map (fn [x] (Integer/parseInt x)) %) groups))
+
+(defn sum-groups [grouped]
+  (map #(reduce + 0 %) grouped))
+
+(defn file-lines [file-name]
+  (clojure.string/split-lines (slurp file-name)))
+
+(defn sort-desc [coll]
+  (reverse (sort coll)))
+
+(defn elfs []
+  (-> input-file file-lines grouped-by-calories to-numbers sum-groups sort-desc))
+
+(defn first-elf [elfs]
+  (first elfs))
+
+(defn first-three-elfs-sum [elfs]
+  (reduce + (take 3 elfs)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  "Day 1"
+  [& _]
+  (let [elfs (elfs)]
+    (println (str "Highest elf: " (first-elf elfs)))
+    (println (str "first three: " (first-three-elfs-sum elfs)))))
+
--- test/aoc_2022/day1_test.clj
+++ test/aoc_2022/day1_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [aoc-2022.day1 :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest grouping-tests
+  (is (= (grouped-by-calories ["1"]) [["1"]]))
+  (is (= (grouped-by-calories ["1" "2" "" "3"]) [["1" "2"] ["3"]])))
+
+(deftest to-numbers-tests
+  (is (= (to-numbers [["1"]]) [[1]]))
+  (is (= (to-numbers [["1" "2" "3"] ["5"]]) [[1 2 3] [5]])))
+
+(deftest summation-tests
+  (is (= (sum-groups [[1]]) [1]))
+  (is (= (sum-groups [[1 2]]) [3]))
+  (is (= (sum-groups [[2 2] [1 1 1 1]]) [4 4])))
+
+(deftest sort-desc-tests
+  (is (= (sort-desc [1 2 3]) [3 2 1]))
+  (is (= (sort-desc [1 4 2 56 2]) [56 4 2 2 1])))
+
+(deftest elfs-tests
+  (is (= (first-elf [1]) 1))
+  (is (= (first-elf [4 3 1]) 4))
+  (is (= (first-three-elfs-sum [3 2 1]) 6))
+  (is (= (first-three-elfs-sum [10 4 3 2 2 1]) 17)))
