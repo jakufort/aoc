b769755ecfb5d8ff8fc4ce64960f0b13e12f90ba
Jakub Fortunka <jakufort@gmail.com>
2022-12-03 11:49:14 +0100

    day 3 part 1

---
+++ src/aoc_2022/day3.clj
@@ -0,0 +1,36 @@
+(ns aoc-2022.day3
+  (:gen-class)
+  (:require [aoc-2022.utils :as utils]
+            [clojure.set :refer [intersection]]))
+
+; line processing
+(defn line-split [line]
+  (let [size (count line)
+        half (/ size 2)]
+    [(subs line 0 half) (subs line half size)]))
+
+(defn to-sets [[left right]]
+  [(set left) (set right)])
+
+(defn common-element [[left right]]
+  (first (intersection left right)))
+
+(defn common-item [line]
+  (common-element (to-sets (line-split line))))
+
+; priority compute
+(defn offset [^Character character]
+  (if (Character/isLowerCase character) 96 38))
+
+(defn priority [character]
+  (- (int character) (offset character)))
+
+; solutions
+(defn item-in-both-compartments-priorities [lines]
+  (reduce + (map priority (map common-item lines))))
+
+(defn -main
+  "Day 3"
+  [& _]
+  (let [lines (utils/file-lines "./resources/day3/input")]
+    (println (str "part1: " (item-in-both-compartments-priorities lines)))))
---
+++ test/aoc_2022/day3_test.clj
@@ -0,0 +1,34 @@
+(ns aoc-2022.day3-test
+  (:require [aoc-2022.day3 :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest split-line-in-half
+  (is (= (line-split "abcd") ["ab" "cd"]))
+  (is (= (line-split "BhshUIhjkjfA") ["BhshUI" "hjkjfA"])))
+
+(deftest unique-tests
+  (is (= (to-sets ["aba" "abc"]) [#{\a \b} #{\a \b \c}]))
+  (is (= (to-sets ["abaaaaa" "bbdbc"]) [#{\a \b} #{\b \d \c}])))
+
+(deftest common-element-tests
+  (is (= (common-element [#{\a} #{}]) nil))
+  (is (= (common-element [#{\a} #{\a}]) \a)))
+
+(deftest common-item-tests
+  (is (= (common-item "abac") \a))
+  (is (= (common-item "vJrwpWtwJgWrhcsFMMfFFhFp") \p))
+  (is (= (common-item "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") \L)))
+
+(deftest priority-tests
+  (is (= (priority \a)) 1)
+  (is (= (priority \b)) 2)
+  (is (= (priority \z)) 26)
+  (is (= (priority \A)) 27)
+  (is (= (priority \B)) 28)
+  (is (= (priority \Z)) 52))
+
+(deftest item-in-both-compartments-priorities-tests
+  (is (=
+        (item-in-both-compartments-priorities ["vJrwpWtwJgWrhcsFMMfFFhFp" "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"])
+        54)))
+
